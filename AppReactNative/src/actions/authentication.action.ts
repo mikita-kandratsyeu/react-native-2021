2f42ea5d7fa718460c6f339ada0856a4654d0fca
Mikita Kandratsyeu <Mikita_Kandratsyeu1@epam.com>
2021-03-17 21:42:07 +0300

    fix a few typos

--- AppReactNative/src/actions/authentication.action.ts
+++ AppReactNative/src/actions/authentication.action.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-export const authentication = (loginname: string, password: string) =>
+export const authentication = (login: string, password: string) =>
   axios({
-    method: 'post',
+    method: 'POST',
     url: 'http://35.231.209.61/index.php?rt=a/account/login',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    data: `loginname=${loginname}&password=${password}`,
+    data: `loginname=${login}&password=${password}`,
   })
     .then(res => Promise.resolve(res.data))
     .catch(err => console.error(err));
