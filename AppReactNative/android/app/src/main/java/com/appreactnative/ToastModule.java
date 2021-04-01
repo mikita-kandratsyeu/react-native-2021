51a528512b8b8123f12cb82c5e9646875b2cb9e1
Mikita Kandratsyeu <Mikita_Kandratsyeu1@epam.com>
2021-04-01 14:09:10 +0300

    add Custom Tost View for Android

--- AppReactNative/android/app/src/main/java/com/appreactnative/ToastModule.java
+++ AppReactNative/android/app/src/main/java/com/appreactnative/ToastModule.java
@@ -1,5 +1,10 @@
 package com.appreactnative;
 
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -39,6 +44,19 @@ public class ToastModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void showText(String message, int duration) {
-        Toast.makeText(getReactApplicationContext(), message, duration).show();
+        LayoutInflater inflater = getCurrentActivity().getLayoutInflater();
+
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) getCurrentActivity().findViewById(R.id.custom_toast_container));
+
+        TextView text = (TextView) layout.findViewById(R.id.custom_toast_text);
+        text.setText(message);
+
+        Toast toast = new Toast(getReactApplicationContext());
+
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(duration);
+        toast.setView(layout);
+        toast.show();
     }
 }
